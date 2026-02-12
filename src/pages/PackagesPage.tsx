71b1400ce706f4926c1567a44b3917ee57472acc
KylanGillam <kylan@founditmarketing.com>
2026-02-12 09:51:37 -0600

    Update PackagesPage buttons to link to Rinsed checkout form

--- src/pages/PackagesPage.tsx
+++ src/pages/PackagesPage.tsx
@@ -84,7 +84,7 @@ const PackagesPage: React.FC = () => {
                                     )}
 
                                     <a
-                                        href="/buy-now"
+                                        href="https://cavewavecw.app.rinsed.co/checkout_forms/1"
                                         className={`block w-full text-center py-4 rounded-full font-black uppercase tracking-widest text-white shadow-lg bg-gradient-to-r ${pkg.color} bg-[length:200%_auto] hover:bg-right hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300`}
                                     >
                                         Choose {pkg.name}
